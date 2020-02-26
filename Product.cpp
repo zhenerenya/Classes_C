87ba9473618ba625d5dd2e1cf664627eecd3b8c7
zhenerenya <56069186+zhenerenya@users.noreply.github.com>
2020-02-26 23:09:19 +0300

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -6,7 +6,7 @@ Product::Product(char name[], double price, int number)
 	this->price = price;
 	this->number = number;
 }
- double Product::foo(double procent) //определяет стоимость товара исходя из заданного процента прибыли
+ double Product::calculate_expected_price()(double procent) //определяет стоимость товара исходя из заданного процента прибыли
 {
 	return this->price + this->price * (procent / 100.);
 }
