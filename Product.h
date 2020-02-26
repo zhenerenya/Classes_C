11af9ceec19656f6a41fa60484ced58bc76254e8
zhenerenya <56069186+zhenerenya@users.noreply.github.com>
2020-02-26 23:08:42 +0300

    Update Product.h

--- Product.h
+++ Product.h
@@ -6,12 +6,12 @@ using namespace std;
 
 class Product
 {
-	char name[20]; //наименование
+	string name; //наименование
 	double price; //закупочная цена
 	int number; //количество товара
 
 public:
-	Product(char name[], double price, int number);
+	Product(string name, double price, int number);
 	~Product();
 	double foo(double procent); //определяет стоимость товара исходя из заданного процента прибыли
 	void print(); //печатает параметры
